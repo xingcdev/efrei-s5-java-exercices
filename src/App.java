3684b39321d0563ebf36bd8519f7594e502b32aa
Xing C <60323726+xingcdev@users.noreply.github.com>
2021-10-11 22:11:05 +0200

    ajout main dans la class Livre

--- src/App.java
+++ src/App.java
@@ -1,13 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        Livre bouquin1 = new Livre("Fumio Sasaki", "L'essentiel et rien d'autre");
-        Livre bouquin2 = new Livre("Walter Isaacson", "Steve Jobs");
-        bouquin1.setNbPages(200);
-        bouquin2.setNbPages(900);
-        System.out.println("bouquin1 : " + bouquin1.getAuteur());
-        System.out.println("nombre de page : " + bouquin1.getNbPages());
-        System.out.println("bouquin2 : " + bouquin2.getAuteur());
-        System.out.println("nombre de page : " + bouquin2.getNbPages());
-        System.out.println("Le nombre de page total : " + (bouquin1.getNbPages() + bouquin2.getNbPages()));
+
     }
 }
